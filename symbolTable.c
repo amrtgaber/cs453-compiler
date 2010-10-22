73705bdbed239cf3181397ece261a2b56a23bbd3
Amr Gaber <amrtgaber@gmail.com>
2010-10-22 01:49:56 -0700

    function symbol under construction

--- symbolTable.c
+++ symbolTable.c
@@ -1,15 +1,15 @@
 /* File: symbalTable.c 
  * Author: Amr Gaber
  * Created: 19/10/2010
- * Last Modified: 20/10/2010
+ * Last Modified: 21/10/2010
  * Purpose: Symbol table for use with the C-- compiler.
  */
 
 #include "symbolTable.h"
 #include "utilities.h"
 
-extern _errorMessage[ERROR_MSG_SIZE];
 SymbolTable *_stack = NULL;
+Symbol *_currentFunction = NULL;
 
 /* Function: recall
  * Parameters: char *identifier
@@ -21,41 +21,97 @@ Symbol *recall(char *identifier) {
 	if (!_stack)
 		ERROR("Recall called on empty stack.", __LINE__, FALSE);
 
-	Symbol *symbol = NULL, *currSymbol = NULL;
+	Symbol *currSymbol = NULL;
 	SymbolTable currTable = NULL;
 	
 	for(currTable = _stack; currTable; currTable = currTable->below)
-		for (currSymbol = currTable->head; currSymbol; currSymbol = currSymbol->next)
+		for(currSymbol = currTable->listHead; currSymbol; 
+				currSymbol = currSymbol->next)
 			if (strcmp(currSymbol->identifier, identifier) == 0)
 				return currSymbol;
 	
 	return NULL;
 }
 
+/* Function: recallLocal
+ * Parameters: char *identifier
+ * Description: Recalls a symbol from the symbol table on top of the stack.
+ * Returns: A pointer to the symbol if found, NULL otherwise.
+ * Preconditions: The stack must not be empty.
+ */
+Symbol *recallLocal(char *identifier) {
+	if (!_stack)
+		ERROR("RecallLocal called on empty stack.", __LINE__, FALSE);
+
+	Symbol *currSymbol = NULL;
+	
+	for(currSymbol = _stack->listHead; currSymbol; currSymbol = currSymbol->next)
+		if (strcmp(currSymbol->identifier, identifier) == 0)
+			return currSymbol;
+
+	return NULL;
+}
+
 /* Function: insert
- * Parameters: char *identifier, Type type, Value value
+ * Parameters: char *identifier, Type type, Value value, Parameter 
  * Description: Inserts a symbol into the symbol table.
  * Returns: A pointer to the inserted symbol.
  * Preconditions: The stack must not be empty.
  */
-Symbol *insert(char *identifier, Type type, Value value) {
+Symbol *insert(char *identifier, Type type, Value value, FunctionType functionType) {
 	if (!_stack)
 		ERROR("Insert called on emtpy stack.", __LINE__, FALSE);
 
-	Symbol *toInsert = null;
+	Symbol *toInsert = NULL;
 	SymbolTable currTable = _stack;
 	
 	if (!(toInsert = malloc(sizeof(Symbol))))
-		ERROR(NULL, __LINE__, TRUE); 				// out of memory
+		ERROR("", __LINE__, TRUE); 				// out of memory
 	
 	toInsert->identifier = strdup(identifier);
 	toInsert->type = type;
 	toInsert->value = value;
+	toInsert->functionType = functionType;
+	toInser->parameterListHead = NULL;
 	
-	toInsert->next = currTable->head;
-	currTable->head = toInsert;
+	toInsert->next = currTable->listHead;
+	currTable->listHead = toInsert;
+	
+	if (functionType)
+		_currentFunction = toInsert;
 	
 	return toInsert;
+}
+
+/* Function: addParameter
+ * Parameters: Type type
+ * Description: Adds a parameter to the most recently inserted function then 
+ *					declares it as a local variable on the local stack.
+ * Returns: void
+ * Preconditions: The most recently inserted symbol must have been a function.
+ */
+void addParameter(char *identifier, Type type) {
+	Parameter *newParameter = NULL, *currParameter = NULL;
+	
+	if (!(newParameter = malloc(sizeof(Parameter))))
+		ERROR("", __LINE__, TRUE);				// out of memory
+	
+	newParameter->type = type;
+	newParameter->next = NULL;
+	
+	// if the parameter list is null add at head, if not add at end
+	if (!(_currentFunction->parameterListHead)) {
+		_currentFunction->parameterListHead = newParameter;
+	} else {
+		currParameter = _currentFunction->parameterListHead;
+		
+		while(currParameter->next)
+			currParameter = currParameter->next;
+			
+		currParameter->next = newParameter;
+	}
+	
+	insert(identifier, type, NULL, NULL);
 }
 
 /* Function: pushSymbolTable
@@ -68,9 +124,9 @@ void pushSymbolTable() {
 	SymbolTable *newTable = NULL;
 	
 	if (!(newTable = malloc(sizeof(SymbolTable))))
-		ERROR(NULL, __LINE__, TRUE);				// out of memory
+		ERROR("", __LINE__, TRUE);				// out of memory
 	
-	newTable->head = NULL;
+	newTable->listHead = NULL;
 	newTable->below = _stack;
 	_stack = newTable;
 }
@@ -89,8 +145,8 @@ void popSymbolTable() {
 	SymbolTable *newStackTop = _stack->below;
 	
 	// if the symbol table at the top has symbols, free them first
-	if (_stack->head) {
-		for(rear = _stack->head, front = rear->next; front; front = front->next) {
+	if (_stack->listHead) {
+		for(rear = _stack->listHead, front = rear->next; front; front = front->next) {
 			free(rear->identifier);
 			free(rear);
 			rear = front;
--- utilities.h
+++ utilities.h
@@ -1,9 +1,8 @@
-/*
- * File: utilities.h
+/* File: utilities.h
  * Author: Amr Gaber
  * Created: 11/4/2010
- * Last Modified: 20/10/2010
- * Purpose: Stores utility data
+ * Last Modified: 21/10/2010
+ * Purpose: Stores utility data.
  */
  
 #ifndef __UTILITIES_H__
@@ -16,8 +15,10 @@
 
 #define FALSE 			0
 #define TRUE 			1
-#define ERROR_MSG_SIZE 	100
 
-void ERROR(char *message, unsigned int lineNum, char perrorFlag);
+/* void ERROR(char *message, unsigned int lineNum, char systemError) -- prints
+ * error message then exits program with status -1
+ */
+void ERROR(char *message, unsigned int lineNum, char systemError);
 
 #endif
