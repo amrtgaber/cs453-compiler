8499bec46ad8c7e462a4cfe0307782eaa40a6913
Amr Gaber <amrtgaber@gmail.com>
2010-11-04 19:43:44 -0700

    working on syntax trees and three address code

---
+++ code.c
@@ -0,0 +1,33 @@
+/* File: code.c 
+ * Author: Amr Gaber
+ * Created: 4/11/2010
+ * Last Modified: 4/11/2010
+ * Purpose: Contains functions for the three address codes.
+ */
+
+#include "utilities.h"
+#include "code.h"
+
+Code *createCode(Opcode opcode, Symbol *source1, Symbol *source2, Symbol *destination {
+	
+	Code *newCode = NULL;
+	
+	if (!(newCode = malloc(sizeof(Code))))
+		ERROR("", __LINE__, TRUE);				// out of memory
+		
+	newCode->opcode = opcode;
+	newCode->source1 = source1;
+	newCode->source2 = source2;
+	newCode->destination = destination;
+	newCode->next = NULL;
+	
+	return newCode;
+}
+
+void destroyCode(Code *code) {
+	if (!code)
+		return;
+	
+	destroyCode(code->next);	
+	free(code);
+}
